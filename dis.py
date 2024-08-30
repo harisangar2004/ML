6fd36b9796ae1f38e6113fd32fe856020dc0f12d
harisangar2004 <122169178+harisangar2004@users.noreply.github.com>
2024-08-30 23:32:17 +0530

    Update dis.py

--- dis.py
+++ dis.py
@@ -1,4 +1,61 @@
-for i in range(len(best_set)):                                                                                                                                                         
+import csv                                                                                                                                                                                 
+test_set = [["sunny",85,85,"FALSE","no"],                                                                                                                                                  
+            ["sunny",80,90,"TRUE","no"],                                                                                                                                                   
+            ["overcast",83,86,"FALSE","yes"],                                                                                                                                              
+            ["rainy",70,96,"FALSE","yes"],                                                                                                                                                 
+            ["rainy", 68, 80,"FALSE","yes"]]                                                                                                                                               
+print("-"*100)                                                                                                                                                                             
+print("{} -> \n{}".format(1, test_set))                                                                                                                                                    
+print("\n{} ->".format(2))                                                                                                                                                                 
+def find_s(test_set):                                                                                                                                                                      
+    hypothesis = [0] * len(test_set[0])                                                                                                                                                    
+    for i in range(len(test_set)):                                                                                                                                                         
+        if test_set[i][-1] == "yes":                                                                                                                                                       
+            positive = test_set[i]                                                                                                                                                         
+            for j in range(len(positive)):                                                                                                                                                 
+                if hypothesis[j] == 0:                                                                                                                                                     
+                    hypothesis[j] = positive[j]                                                                                                                                            
+                elif hypothesis[j] == positive[j]:                                                                                                                                         
+                    continue                                                                                                                                                               
+                else:                                                                                                                                                                      
+                    hypothesis[j] = "?"                                                                                                                                                    
+        print(i+1, " Hypothesis : ", hypothesis[:-1])                                                                                                                                      
+    print("\n{} ->".format(3))                                                                                                                                                             
+    print("General Hypothesis : ", hypothesis[:-1])                                                                                                                                        
+                                                                                                                                                                                           
+find_s(test_set)                                                                                                                                                                           
+def instances(test_set):                                                                                                                                                                   
+    print("\n{} ->".format(4))                                                                                                                                                             
+    s = []                                                                                                                                                                                 
+    for i in range(len(test_set)-1):                                                                                                                                                       
+        a = set()                                                                                                                                                                          
+        for j in range(len(test_set[0])):                                                                                                                                                  
+            a.add(test_set[j][i])                                                                                                                                                          
+        s.append(len(a))                                                                                                                                                                   
+    i1 = 1                                                                                                                                                                                 
+    for i in s:                                                                                                                                                                            
+        i1 = i1*i                                                                                                                                                                          
+    i2 = 1                                                                                                                                                                                 
+    for i in s:                                                                                                                                                                            
+        i2 = i2*(i+2)                                                                                                                                                                      
+    i3 = 1                                                                                                                                                                                 
+    for i in s:                                                                                                                                                                            
+        i3 = i3*(i+1)                                                                                                                                                                      
+    i3 += 1                                                                                                                                                                                
+    print("Total Instances : ", i1)                                                                                                                                                        
+    print("Syntatically different : ", i2)                                                                                                                                                 
+    print("Semanticallly different : ", i3)                                                                                                                                                
+    print("-"*100)                                                                                                                                                                         
+instances(test_set)                                                                                                                                                                        
+                                                                                                                                                                                           
+def discritize(best_set):                                                                                                                                                                  
+    it = min(x[1] for x in best_set)                                                                                                                                                       
+    at = max(x[1] for x in best_set)                                                                                                                                                       
+    ih = min(x[2] for x in best_set)                                                                                                                                                       
+    ah = max(x[2] for x in best_set)                                                                                                                                                       
+    ti = (at-it)/5                                                                                                                                                                         
+    hi = (ah-ih)/5                                                                                                                                                                         
+    for i in range(len(best_set)):                                                                                                                                                         
         t = best_set[i][1]                                                                                                                                                                 
         if t >= it and t <= (it + 1*ti):                                                                                                                                                   
             best_set[i][1] = str(it) + "-" + str(it + 1*ti)                                                                                                                                
